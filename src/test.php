66c3383caa2313055af3c31e593266c82b14b786
oi！！看这里！ <73208658+areCodeOI@users.noreply.github.com>
2025-09-17 03:44:17 +0800

    完善脚本内容 test.php

--- src/test.php
+++ src/test.php
@@ -1,1 +1,13 @@
+<?php
+namespace test;
 
+use Controller\Attr\Rule;
+use App\BaseApp;
+use Utils\Logger;
+
+class test extends BaseApp {
+	#[Rule('/^t$/')]
+	public function t() {
+		Logger::error('我被载入了！！');
+	}
+}
